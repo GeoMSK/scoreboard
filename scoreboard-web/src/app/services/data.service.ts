25c3239429bccca84375ef11393b944f4fbc6a94
George Mantakos <GeoMSKg@gmail.com>
2019-03-26 20:09:56 +0200

    ui build config changes

--- scoreboard-web/src/app/services/data.service.ts
+++ scoreboard-web/src/app/services/data.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Entry } from "../model/entry";
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -14,13 +15,15 @@ interface FlagResult {
   success: Boolean
 }
 
+const api = environment.apiAddress;
+
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
 
-  private entriesUrl = "http://localhost:8080/api/entries";
-  private flagUrl = "http://localhost:8080/api/submitFlag";
+  private entriesUrl = `${api}/api/entries`;
+  private flagUrl = `${api}/api/submitFlag`;
 
   constructor(private http: HttpClient) { }
 
@@ -29,7 +32,7 @@ export class DataService {
   }
 
   submitFlag(flag: String, name: String): Observable<HttpResponse<any>> {
-    var resp: Observable<HttpResponse<any>> = this.http.post<any>(this.flagUrl, {flag: flag, name: name}, httpOptions);
+    var resp: Observable<HttpResponse<any>> = this.http.post<any>(this.flagUrl, { flag: flag, name: name }, httpOptions);
     return resp;
   }
 
